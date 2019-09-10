85bc0184d50eaeb3253474c3a8cac3a8b1a2ddbc
joonson22 <johnsonlai22@gmail.com>
2019-09-10 14:58:26 -0700

    Added Auth and Protected routes for when the user is signed in

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import GreetingContainer from './greeting/greeting_container'
+import React from 'react';
+import GreetingContainer from './greeting/greeting_container';
+import LoginFormContainer from './session_form/login_form_container';
+import SignupFormContainer from './session_form/signup_form_container'
+import {Route} from 'react-router-dom'
+import {AuthRoute, ProtectedRoute} from '../util/route_util'
 const App = () => (
     <div>
         <header>
             <h1>Internet Flicks</h1>
             <GreetingContainer />
         </header>
+        <AuthRoute path='/login' component={LoginFormContainer} />
+        <AuthRoute path='/signup' component={SignupFormContainer} />
     </div>
 )
 
