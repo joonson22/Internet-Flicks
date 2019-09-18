939136abba1346e21c9ad1c0ef1c1795fa9c4407
joonson22 <johnsonlai22@gmail.com>
2019-09-17 17:36:30 -0700

    Fixed popout and created popout component

--- frontend/components/movies/movie_item_popout.jsx
+++ frontend/components/movies/movie_item_popout.jsx
@@ -4,15 +4,21 @@ class MovieItemPopout extends React.Component {
     render() {
         return(
             <div className='movie-popout'>
-                <div className='popout-content'>
-                    <h1>{this.props.movie.title}</h1>
-                    <h3>{this.props.movie.rating}</h3>
-                    <h3>{this.props.movie.body}</h3>
-                    <h3>{this.props.movie.year}</h3>
+                <div className='popout-bg'>
+                    <div className='popout-left'>
+                        <h1>{this.props.movie.title}</h1>
+                        <h3>{this.props.movie.rating}</h3>
+                        <h3>{this.props.movie.body}</h3>
+                        <h3>{this.props.movie.year}</h3>
+                    </div>
+                    <div className='popout-right'>
+                    <video width='251' height='141' controls >
+                        <source src={this.props.movie.videoUrl} type='video/mp4' />
+                    </video>
+                    </div>
                 </div>
-                <video width='251' height='141' controls onMouseLeave={this.handleLeave.bind(this)}>
-                    <source src={this.props.movie.videoUrl} type='video/mp4' />
-                </video>
+                <div className='popout-content'>
+                </div>
             </div>
         )
     }
