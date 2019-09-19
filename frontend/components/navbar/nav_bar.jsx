9b2559717ad4bd442eb1d0d93202144961b6f136
joonson22 <johnsonlai22@gmail.com>
2019-09-19 15:39:13 -0700

    Added logout button to navbar

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -2,23 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import {withRouter} from 'react-router'
 
-const NavBar = (props) => (
-    // props.navbar ? (
-    //     <div className='nav-bar-container'>
-    //         {/* <a href="#"><img src="https://fontmeme.com/permalink/190911/42359bbf84e020be8f811016380611ac.png" alt="netflix-font" border="0"></img></a>
-    //         {this.props.user ? (
-    //             <button>
-    //                 Logout
-    //             </button>
-    //         ) : (
+class NavBar extends React.Component {
 
-    //             <Link to='login' className='splash-login-link'>Sign in</Link>
-    //         )} */}
-    //     </div>
-    // ) : ("")
-
-    <div></div>
-
-)
+    render() {
+        return (
+           this.props.navbar ? (
+               <div className='nav-bar-container'>
+                   <Link to='/movies'>
+                        <img src="https://fontmeme.com/permalink/190911/42359bbf84e020be8f811016380611ac.png" alt="netflix-font" border="0"></img>
+                   </Link>
+                    {this.props.user ? (
+                        <button onClick={this.props.logout}>Logout</button>
+                    ) : (
+                        <Link to = 'login' className = 'splash-login-link'>Sign in</Link>
+                    ) }
+               </div>
+           ) : ("")
+        )
+    }
+}
 
 export default NavBar;
+
+// const NavBar = (props) => (
+//     props.navbar ? (
+//         <div className='nav-bar-container'>
+//             <a href="#"><img src="https://fontmeme.com/permalink/190911/42359bbf84e020be8f811016380611ac.png" alt="netflix-font" border="0"></img></a>
+//             {this.props.user ? (
+//                 <button>
+//                     Logout
+//                 </button>
+//             ) : (
+//                 <Link to='login' className='splash-login-link'>Sign in</Link>
+//             )}
+//         </div>
+//     ) : ("")
+
+// )
+
+// export default NavBar;
--- frontend/components/navbar/nav_bar_container.js
+++ frontend/components/navbar/nav_bar_container.js
@@ -1,13 +1,20 @@
 import NavBar from './nav_bar';
 import {connect} from 'react-redux'
+import {logout} from '../../actions/session_actions'
 
 
 const msp = state => {
     return ({
         user: state.entities.users[state.session.id],
         navbar: state.ui.navbar,
-        user: state.session
+        user: state.session.id
     })
 }
 
-export default connect(msp)(NavBar)
+const mdp = dispatch => {
+    return({
+        logout: () => dispatch(logout())
+    })
+}
+
+export default connect(msp,mdp)(NavBar)
